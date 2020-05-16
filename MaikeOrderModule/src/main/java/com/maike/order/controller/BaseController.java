8377be0203a8470a8d95ffc90f743d4fe9db09ca
zhuangCs <zhuangCs@163.com>
2020-05-16 21:45:30 +0800

    1

---
+++ MaikeOrderModule/src/main/java/com/maike/order/controller/BaseController.java
@@ -0,0 +1,5 @@
+package com.maike.order.controller;
+
+public class BaseController {
+
+}
---
+++ MaikeOrderModule/src/main/java/com/maike/order/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.maike.order.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * order统一登陆入口
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController extends BaseController {
+    @RequestMapping(value = "/apply" , method = { RequestMethod.POST})
+    public String apply(){
+
+        return "success";
+    }
+}
